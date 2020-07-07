0a1d987641428501921a669af3d25375e09e597b
Vihaga Yohan <vihagayohan94@gmail.com>
2020-07-07 08:40:11 +0530

    1_Initial commit

---
+++ About.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+const About = ()=>{
+    return(
+        <div>
+            <h1>About page</h1>
+        </div>
+    )
+}
+
+export default About;
---
+++ src/Nav.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+const Nav = ()=>{
+    return(
+        <div>
+            <h1>Logo</h1>
+            <ul>
+                <li>About</li>
+                <li>Shop</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Nav;
